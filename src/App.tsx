b93699434013f3ade2eaf019b9b16952b33114b3
Chris Mendez <mendecj812@gmail.com>
2024-02-01 20:32:14 -0600

    feat: add navbar with links to home & favorites
    Add navbar with React Router NavLinks for Home & Favorites page.
    Also remove Header component since the navbar sits at the root
    of the app and is visible at the home & favs page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes, NavLink } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Favorites } from "./pages/Favorites";
 
 function App() {
   return (
     <>
-      <nav>
-        <ul>
+      <nav className="px-4 py-8">
+        <ul className="flex items-center justify-between md:mx-auto md:max-w-6xl">
           <li>
-            <Link to="/">Home</Link>
+            <NavLink to="/" className="text-xl font-bold">
+              Latte Logs
+            </NavLink>
           </li>
           <li>
-            <Link to="/favorites">Favorites</Link>
+            <NavLink
+              to="/favorites"
+              className="text-xl"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? "underline" : "",
+              })}
+            >
+              Favorites
+            </NavLink>
           </li>
         </ul>
       </nav>
--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export function Header() {
-  return (
-    <header>
-      <h1>Latte Logs</h1>
-    </header>
-  )
-} 
