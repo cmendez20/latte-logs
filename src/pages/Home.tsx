c60f8abf08b78e420ba1593a5c1de0b0d4bee8c3
Chris Mendez <mendecj812@gmail.com>
2024-02-01 21:04:12 -0600

    build: add supabase npm package and import client

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,10 @@
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+// const supabase = createClient(
+
+// );
+
 export function Home() {
   return (
     <>
